43ce66b16e5c6b60d68df3ce8456f22db9e40298
Crainbowi <wenzhen0016@163.com>
2020-03-30 16:48:42 +0800

    第二次提交

--- src/main/java/com/atwenzhen/community/controller/GreetingController.java
+++ src/main/java/com/atwenzhen/community/controller/GreetingController.java
@@ -14,7 +14,6 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name") String name, Model model) {
-        model.addAttribute("name", name);
         return "greeting";
     }
 }
