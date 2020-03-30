ed191ec1ae932817bbe6a3a1ecf45ebb288d7595
wz_glb <wz_glb@qq.com>
2020-03-30 16:30:27 +0800

    init repo

---
+++ src/main/java/com/atwenzhen/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.atwenzhen.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/atwenzhen/community/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.atwenzhen.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author wenzhen
+ * @create 2020-03-30 16:00
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
